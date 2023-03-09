3818915b70a809a912e8642965d1ca56f086aabe
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-09 14:08:19 -0600

    Feedback

--- exercise8.js
+++ exercise8.js
@@ -1,30 +1,19 @@
-// Given an array of size N, move all zeroes contained in the array to the
-// end while  preserving the relative order of all other elements.
-// - Do not use a separate array
-// - Do not use splice
-
-// I decided to use the -sort- method in this method,
-// the sorting order is defined by a comparison function
-// that takes two elements of the array as parameters (a and b).
 function moveZeros(arr) {
-  arr.sort((a, b) => {
-    // If -a- is 0, it returns 1, which means that it should be placed
-    // after -b- in the sorted array.
-    if (a === 0) {
-      return 1;
+  let nextNonZeroIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      // Swap the current element with the next non-zero element
+      let currentNonZeroElement = arr[i];
+      arr[i] = arr[nextNonZeroIndex];
+      arr[nextNonZeroIndex] = currentNonZeroElement;
+      // Increment the index where the next non-zero element should be inserted
+      nextNonZeroIndex++;
     }
-    // If -b- is 0, it returns -1, which means that it should be placed
-    // before -a- in the sorted array.
-    else if (b === 0) {
-      return -1;
-    }
-    // If neither a nor b is 0, it returns 0, which means that their
-    // relative order in the sorted array remains unchanged.
-    return 0;
-  });
+  }
   return arr;
-  // After the sort, all zeros will be at the end of the array, while
-  // preserving the relative order of all other elements.
 }
 
 console.log(moveZeros([2, 0, 4, 5, 0, 2, 0, 0, 3]));
+// Output: [2, 4, 5, 2, 3, 0, 0, 0, 0]
+console.log(moveZeros([2, 0, 4, 5, 0, 2, 0, 0, 3]));
+// Output: [2, 4, 5, 2, 3, 0, 0, 0, 0];
