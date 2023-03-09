011fb2e780ad67ceef2cada1f8da50c0c384cf9b
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-09 11:57:40 -0600

    Feedback

--- exercise2.js
+++ exercise2.js
@@ -1,51 +1,47 @@
-// Reverse a string in JavaScript (1 hours)
-// Do not use split.reverse.join
-// *Create an instance method for all strings that returns
-// the reversed string:
-//      "hello world".reverse(); // ldrow olleh
-// **Create an instance method for all strings that returns
-// the reversed string, but keeps the order of the words:
-//   	"hello world".reverseWords(); // olleh ldrow
+// // Reverse a string in JavaScript (1 hours)
+// // Do not use split.reverse.join
+// // *Create an instance method for all strings that returns
+// // the reversed string:
+// //      "hello world".reverse(); // ldrow olleh
+// // **Create an instance method for all strings that returns
+// // the reversed string, but keeps the order of the words:
+// //   	"hello world".reverseWords(); // olleh ldrow
 
-// One fast way to do this is by creating a prototype method
-// that will be available to all strings. This can be done by defining
-// a new method on the String.prototype object, all the strings
-// will have access to it:
+// // One fast way to do this is by creating a prototype method
+// // that will be available to all strings. This can be done by defining
+// // a new method on the String.prototype object, all the strings
+// // will have access to it:
+
+// Define the `reverse` method for all string instances
 String.prototype.reverse = function () {
-  // This object is defined here to simplify the operations
-
-  let inputObject = {
-    input: this,
-    output: "",
-  };
-  while (inputObject.input.length >= 1) {
-    // The last letter of the input will be added (by the right)
-    // to the inputObject.output string.
-    inputObject.output += inputObject.input[inputObject.input.length - 1];
-    // That letter will be removed from the inputObject.input
-    inputObject.input = inputObject.input.substring(
-      0,
-      inputObject.input.length - 1
-    );
+  let output = "";
+  // Iterate over the characters of the original string in reverse order
+  for (let i = this.length - 1; i >= 0; i--) {
+    // Append each character to the `output` string
+    output += this[i];
   }
-  return inputObject.output;
+  // Return the reversed string
+  return output;
 };
-// console.log("Hello world".reverse());
 
-// To create the second string method, it is possible to reuse
-// some ideas as well as the .reverse() method
-// "hello world".reverseWords(); // olleh ldrow
-
+// Define the `reverseWords` method for all string instances
 String.prototype.reverseWords = function () {
-  let inputObject = {
-    input: this.split(" "),
-    output: "",
-  };
-
-  for (word of inputObject.input) {
-    inputObject.output += word.reverse() + " ";
+  // Split the original string into words
+  let words = this.split(" ");
+  let output = "";
+  // Iterate over the words in reverse order
+  for (let i = words.length - 1; i >= 0; i--) {
+    // Append each word to the `output` string
+    output += words[i];
+    // Add a space if not at the last word
+    if (i > 0) {
+      output += " ";
+    }
   }
-  return inputObject.output.substring(0, inputObject.output.length - 1);
+  // Return the reversed words string
+  return output;
 };
 
-console.log("Hello world".reverseWords());
+// Test the `reverse` and `reverseWords` methods
+console.log("hello world".reverse()); // "dlrow olleh"
+console.log("hello world".reverseWords()); // "world hello"
