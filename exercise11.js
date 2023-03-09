7472c21450782963cfd5235814d9a21f6746cc19
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-09 14:29:45 -0600

    Feedback

--- exercise11.js
+++ exercise11.js
@@ -1,5 +1,6 @@
 // Create a function that will return the properties that
 // are different between 2 objects.
+
 let plane = {
   wheels: 4,
   fuel: "Jet A",
@@ -21,14 +22,14 @@ let jet = {
 
 function differenceFinder(obj1, obj2) {
   let differentProperties = {};
-  let propertiesObj1 = Object.getOwnPropertyNames(obj1);
-  let propertiesObj2 = Object.getOwnPropertyNames(obj2);
-  // 1) Properties of object 1 not present in object 2:
+
+  // 1) Properties of object 1
   for (let property in obj1) {
+    // If the property is not in obj2
     if (!propertiesObj2.includes(property)) {
       differentProperties[property] = { Object1: obj1[property], Object2: "" };
     }
-    //Properties present in both object, but with different values:
+    // If the property is in both objects and has different values
     else if (obj1[property] !== obj2[property]) {
       differentProperties[property] = {
         Object1: obj1[property],
@@ -37,19 +38,14 @@ function differenceFinder(obj1, obj2) {
     }
   }
 
-  // 2) Properties of object 2 not present in object 1:
+  // 2) Properties of object 2
   for (let property in obj2) {
+    // If the property is not in obj1
     if (!propertiesObj1.includes(property)) {
       differentProperties[property] = { Object1: "", Object2: obj2[property] };
     }
-    //Properties present in both object, but with different values:
-    else if (obj1[property] !== obj2[property]) {
-      differentProperties[property] = {
-        Object1: obj1[property],
-        Object2: obj2[property],
-      };
-    }
   }
+
   return differentProperties;
 }
 
