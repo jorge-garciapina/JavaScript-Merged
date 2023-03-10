69ca20992a315198123eb7eb88282dcd3489d23b
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-10 14:41:53 -0600

    Feedback

--- exercise30.js
+++ exercise30.js
@@ -1,7 +1,26 @@
-$(document).ready(function () {
-  // This jQuery selectors selects all p elements
-  // whose text content contains the string "Toggle"
-  $('p:contains("Toggle")').click(function () {
-    $(this).next("p").toggle();
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all p elements on the page
+  let paragraphsInPage = document.querySelectorAll("p");
+
+  // Iterate over each p element
+  paragraphsInPage.forEach(function (p) {
+    // If the text content of the p element contains the word "Toggle"
+    if (p.textContent.indexOf("Toggle") !== -1) {
+      // Add a click event listener to the p element
+      p.addEventListener("click", function () {
+        // Get the next sibling of the clicked p element
+        let adjacentP = p.nextElementSibling;
+        let stylesOfAdjacent = window.getComputedStyle(adjacentP);
+
+        let isHidden = stylesOfAdjacent.display === "none";
+
+        // Hide and Show functionallity
+        if (isHidden) {
+          adjacentP.style.display = "block";
+        } else {
+          adjacentP.style.display = "none";
+        }
+      });
+    }
   });
 });
