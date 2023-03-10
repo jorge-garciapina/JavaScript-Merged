183b8da5bb83c7f4a19b992f6e7b3d82174f77c5
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-10 16:22:57 -0600

    Feedback

--- exercise31.js
+++ exercise31.js
@@ -1,12 +1,35 @@
-$(document).ready(function () {
-  // This selector selects all `p` elements whose text
-  // content contains the string "Toggle"
-  $("p:contains('Toggle')").click(function () {
-    // This selector selects all `p` elements except the
-    // one that was clicked
-    $("p").not(this).next().hide();
-    // This selector selects the next `p` element after
-    // the one that was clicked and toggles its visibility
-    $(this).next().toggle();
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all p elements on the page
+  let paragraphsInPage = document.querySelectorAll("p");
+
+  let currentlyShownAdjacent = null;
+
+  // Iterate over each p element
+  paragraphsInPage.forEach(function (p) {
+    // If the text content of the p element contains the word "Toggle"
+    if (p.textContent.indexOf("Toggle") !== -1) {
+      // Add a click event listener to the p element
+      p.addEventListener("click", function () {
+        // Get the next sibling of the clicked p element
+        let adjacentP = p.nextElementSibling;
+        let stylesOfAdjacent = window.getComputedStyle(adjacentP);
+
+        let isHidden = stylesOfAdjacent.display === "none";
+
+        // Hide the currently shown adjacent element, if any
+        if (currentlyShownAdjacent && currentlyShownAdjacent !== adjacentP) {
+          currentlyShownAdjacent.style.display = "none";
+        }
+
+        // Show the clicked adjacent element
+        if (isHidden) {
+          adjacentP.style.display = "block";
+          currentlyShownAdjacent = adjacentP;
+        } else {
+          adjacentP.style.display = "none";
+          currentlyShownAdjacent = null;
+        }
+      });
+    }
   });
 });
