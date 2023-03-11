a4f8ebda123bce8696f913b33f2dbb9a34bb9429
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-10 19:28:57 -0600

    Feedback

--- exercise34.js
+++ exercise34.js
@@ -1,68 +1,55 @@
-// Write the logic required to represent a bouncing ball.
-// The requirements are:
-//  - Initial height of the ball should be 10 in.
-//  - On first hitting the floor, it should bounce back 9 in.
-//  - On the second bounce, it should go up 8 in. and so on.
-//  - Animate a ball in HTML/CSS using the written logic
-// Initial height of the ball
-// Initial height of the ball in pixels
-// let height = 400;
+let startButton = document.getElementById("start");
+let ball = document.getElementById("ball");
 
-// // Keep track of the number of bounces
-// let bounceCount = 0;
+function movementIntervalsOfElement(elementToAnimate) {
+  let ballHeight = window.getComputedStyle(elementToAnimate).bottom;
+  let numericalValueOfHeight = parseFloat(ballHeight);
+  const initialHeight = 10;
+  let height = initialHeight;
+  let isFalling = true;
+  let initialPosition;
+  let finalPosition;
+  let positions = [];
 
-// $("#activate").click(function () {
-
-//   $("#ball").animate({
-//     bottom: "0px",
-//   });
-// });
-
-// let initial = 400;
-// let counter = 1;
-// let bounce = [];
-
-// while (initial > 0) {
-//   let moveStep = [];
-//   if (counter % 2 !== 0) {
-//     moveStep.push(initial);
-//     moveStep.push(0);
-//     console.log(initial, 0);
-//     initial -= 40;
-//   } else {
-//     moveStep.push(0);
-//     moveStep.push(initial);
-//     console.log(0, initial);
-//   }
-
-//   bounce.push(moveStep);
-//   counter += 1;
-//   //   console.log(initial, counter);
-// }
-
-// console.log(bounce);
-
-const element = document.getElementById("ball");
-// console.log(element.style);
-
-let x = 0;
-let direction = "falling";
-function animate() {
-  //   console.log(x);
-  if (x <= 500 && direction === "falling") {
-    x += 5;
-    console.log(x);
+  while (height > 0) {
+    let partialPosition = [];
+    if (isFalling) {
+      initialPosition = height;
+      finalPosition = 0;
+      isFalling = false;
+      partialPosition.push(initialPosition * (numericalValueOfHeight / 10));
+      partialPosition.push(finalPosition * (numericalValueOfHeight / 10));
+      positions.push(partialPosition);
+    } else {
+      height -= 1;
+      initialPosition = 0;
+      finalPosition = height;
+      isFalling = true;
+      partialPosition.push(initialPosition * (numericalValueOfHeight / 10));
+      partialPosition.push(finalPosition * (numericalValueOfHeight / 10));
+      positions.push(partialPosition);
+    }
   }
-  if (x === 500) {
-    direction = "rising";
-    x -= 5;
-    console.log("ddfdfdf");
-  }
-  if (x <= 500 && direction === "rising") {
-    x -= 5;
-  }
-  element.style.transform = `translateY(${x}px)`;
-  requestAnimationFrame(animate);
+  return positions;
 }
 
-requestAnimationFrame(animate);
+let movementIntervals = movementIntervalsOfElement(ball);
+
+let intervalTime = 5000 / movementIntervals.length; // 5 seconds divided by the number of intervals
+let currentIndex = 0;
+
+function animateUsingIntervals() {
+  setTimeout(function () {
+    currentIndex++;
+    if (currentIndex < movementIntervals.length) {
+      ball.style.bottom = movementIntervals[currentIndex][0] + "px";
+      setTimeout(animateUsingIntervals, intervalTime);
+    }
+  }, intervalTime);
+}
+
+ball.style.transition = "bottom 0.5s linear";
+
+startButton.onclick = () => {
+  animateUsingIntervals();
+};
