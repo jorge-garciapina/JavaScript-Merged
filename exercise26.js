0c9d471213c6a2fc1a973c8361a2b6eff13f6487
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-13 12:39:58 -0600

    Feedback

--- exercise26.js
+++ exercise26.js
@@ -10,14 +10,14 @@
 // is the return statement of the functions (-foo1- -foo2-)
 
 function foo1() {
-  var fn = function () {
+  let fn = function () {
     console.log(this.foo);
   };
   return { foo: "bar" };
 }
 
 function foo2() {
-  var fn = function () {
+  let fn = function () {
     console.log(this.foo);
   };
   return { foo: "baz" };
