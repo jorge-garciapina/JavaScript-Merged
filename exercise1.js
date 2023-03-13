debc584611243a4d1fadf25e34f9e3504fd03e68
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-13 12:03:12 -0600

    Feedback

--- exercise1.js
+++ exercise1.js
@@ -1,18 +1,35 @@
-$(document).ready(function () {
-  $("#main-container").on({
-    mousemove: function (event) {
-      $("#cannon").css("top", event.screenY - 70 + "px");
-      $("#circle").css("top", event.screenY - 70 + "px");
-    },
-    click: function (event) {
-      $("#circle").animate({
-        left: "100vw",
-        top: event.screenY - 70 + "px",
-      });
-    },
+document.addEventListener("DOMContentLoaded", function () {
+  const mainContainer = document.getElementById("main-container");
+  const cannon = document.getElementById("cannon");
+  const circle = document.getElementById("circle");
+  let isAnimating = false;
+
+  mainContainer.addEventListener("mousemove", function (event) {
+    cannon.style.top = event.screenY - 70 + "px";
+    if (!isAnimating) {
+      circle.style.top = event.screenY - 70 + "px";
+    }
   });
 
-  $("#reload").click(function () {
+  mainContainer.addEventListener("click", function (event) {
+    if (!isAnimating) {
+      isAnimating = true;
+      circle.animate(
+        {
+          left: "100vw",
+          top: event.screenY - 70 + "px",
+        },
+        500
+      ).onfinish = function () {
+        isAnimating = false;
+        circle.style.display = "none"; // hide the circle after the animation ends
+      };
+    }
+  });
+
+  const reloadButton = document.getElementById("reload");
+
+  reloadButton.addEventListener("click", function () {
     window.location.reload();
   });
 });
