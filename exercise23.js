038512e6405c9eef19b656ae6112bad5796aed65
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-14 15:46:44 -0600

    Feedback

--- exercise23.js
+++ exercise23.js
@@ -1,47 +1,29 @@
-function randomIntNoDuplicates(min, max) {
-  // -------------- Start: Input validation --------------
-  // Check if min and max are both numbers
-  if (typeof min !== "number" || typeof max !== "number") {
-    throw new Error("Both min and max must be numbers");
-  }
-  // Check if min is less than max
-  if (min >= max) {
-    throw new Error("Min must be less than max");
-  }
-  // --------------  End: Input validation  --------------
+// Using a "Generator Function"
 
-  // -------------- Start: Main functionallity  --------------
-  // Set that will store the values of the numbers in the
-  // range of (min, max)
-  let remainingNumbers = new Set(
-    Array.from({ length: max - min + 1 }, (_, i) => min + i)
+function* uniqueRandomIntGenerator(min, max) {
+  const rangeSize = max - min + 1;
+
+  if (rangeSize <= 0) {
+    throw new Error("Invalid range: max must be greater than min");
+  }
+
+  const availableNumbers = Array.from(
+    { length: rangeSize },
+    (_, index) => index + min
   );
 
-  return function () {
-    if (!remainingNumbers.size) {
-      throw new Error("All numbers have been generated.");
-    }
-    // Now the code generates a random index between 0 and the
-    // size of -remainingNumbers-
-    const randomIndex = Math.floor(Math.random() * remainingNumbers.size);
-    // Now the code extracts a value from -remainingNumbers-
-    const randomNumber = [...remainingNumbers][randomIndex];
-    // The code uses the .delete() method of the Set to delete
-    // the obtained number.
-    remainingNumbers.delete(randomNumber);
-    return randomNumber;
-  };
-  // --------------  End: Main functionallity  --------------
+  while (availableNumbers.length > 0) {
+    const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+    const randomNumber = availableNumbers.splice(randomIndex, 1)[0];
+    yield randomNumber;
+  }
 }
 
-const generate = randomIntNoDuplicates(1, 10);
-console.log(generate());
-console.log(generate());
-console.log(generate());
-console.log(generate());
-console.log(generate());
-console.log(generate());
-console.log(generate());
-console.log(generate());
-console.log(generate());
-console.log(generate());
+// Example usage:
+const min = 1;
+const max = 10;
+const generator = uniqueRandomIntGenerator(min, max);
+
+for (let i = 0; i < max - min + 1; i++) {
+  console.log(generator.next().value);
+}
