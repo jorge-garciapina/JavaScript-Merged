7c3cfa4697e3cee71721237ee866f66d3eee7358
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-04-28 17:51:51 -0600

    Feedback April

--- exercise19.js
+++ exercise19.js
@@ -1,77 +1,60 @@
 function matchString(pattern, string) {
+  // Get the lengths of pattern and string
   const patternLen = pattern.length;
   const stringLen = string.length;
 
-  let output;
-  let oneIsEmpty = patternLen === 0 || stringLen === 0;
-  let patternIsBigger = patternLen > stringLen;
-
-  // If pattern and string are empty, no match
-  if (oneIsEmpty) {
-    return null;
-  } else if (patternIsBigger) {
+  // If pattern or string are empty, return null
+  if (patternLen === 0 || stringLen === 0) {
     return null;
   }
-  let indexInString = 0;
-  while (indexInString <= stringLen) {
-    let currentMatchesFirstInPattern = string[indexInString] === pattern[0];
-    let patterStartsWithWildcar = pattern[0] === "*";
-    if (currentMatchesFirstInPattern || patterStartsWithWildcar) {
+
+  // Iterate through the string, considering each substring of length
+  // equal to the pattern
+  for (
+    let indexInString = 0;
+    indexInString <= stringLen - patternLen;
+    indexInString++
+  ) {
+    // Assume there is a match until proven otherwise
+    let match = true;
+
+    // Iterate through each character in the pattern
+    for (
       let indexInPattern = 0;
-      output = "";
-      while (indexInPattern <= patternLen) {
-        let consecutivesDontMatch =
-          pattern[indexInPattern] !== string[indexInString];
-        let letterInPatternIsNotWildcard = pattern[indexInPattern] !== "*";
-
-        if (consecutivesDontMatch && letterInPatternIsNotWildcard) {
-          break;
-        }
-        output += string[indexInString];
-        // console.log("aqui", pattern[indexInPattern], output);
-        if (output.length === patternLen) {
-          return output;
-        }
-
-        indexInPattern++;
-        indexInString++;
+      indexInPattern < patternLen;
+      indexInPattern++
+    ) {
+      // If the pattern character is not a wildcard and doesn't match the
+      // corresponding character in the string, set match to false and
+      // break the inner loop
+      if (
+        pattern[indexInPattern] !== "*" &&
+        pattern[indexInPattern] !== string[indexInString + indexInPattern]
+      ) {
+        match = false;
+        break;
       }
     }
-    // console.log("Current: ", string[indexInString]);
-    // console.log("output: ", output);
-    indexInString++;
+
+    // If the loop has finished and match is still true, return the matching substring
+    if (match) {
+      return string.substr(indexInString, patternLen);
+    }
   }
+
+  // If no match is found, return null
+  return null;
 }
 
-let pattern1 = "hel*o";
-let string1 = "hello";
-let string2 = "helo";
-
-console.log("1: ", matchString(pattern1, string1)); // Output: hello
-console.log("2: ", matchString(pattern1, string2)); // Output: null
-
-let pattern2 = "*abc";
-let string3 = "abc";
-let string4 = "xabc";
-
-console.log("3: ", matchString(pattern2, string3)); // Output: null
-console.log("4: ", matchString(pattern2, string4)); // Output: xabc
-
-let pattern3 = "a*b*c";
-let string5 = "abc";
-let string6 = "axbrc";
-
-console.log("5: ", matchString(pattern3, string5)); // Output: null
-console.log("6: ", matchString(pattern3, string6)); // Output: axbrc
-
-let pattern5 = "*";
-let string9 = "hello world";
-console.log("7: ", matchString(pattern5, string9)); // Output: h
-
-let pattern6 = "l*o";
-let string10 = "hello world";
-console.log("8: ", matchString(pattern6, string10)); // Output: llo
-
-let pattern7 = "*h";
-let string11 = "h";
-console.log("9: ", matchString(pattern7, string11)); // Output: null
+console.log("1: ", matchString("*y", "rxy")); // Output: "xy"
+console.log("2: ", matchString("xy", "xxy")); // Output: "xy"
+console.log("3: ", matchString("xy", "xyz")); // Output: "xy"
+console.log("4: ", matchString("hel*o", "hello")); // Output: "hello"
+console.log("5: ", matchString("hel*o", "helo")); // Output: null
+console.log("6: ", matchString("*abc", "abc")); // Output: null
+console.log("7: ", matchString("*abc", "xabc")); // Output: "xabc"
+console.log("8: ", matchString("a*b*c", "abc")); // Output: null
+console.log("9: ", matchString("a*b*c", "axbrc")); // Output: "axbrc"
+console.log("10: ", matchString("*", "hello world")); // Output: "h"
+console.log("11: ", matchString("l*o", "hello world")); // Output: "llo"
+console.log("12 ", matchString("*h", "h")); // Output: null
