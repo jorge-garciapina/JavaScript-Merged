16e125f27fcc5eaaf34fe228caca80953f7bff1c
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-05-02 12:24:57 -0600

    Feedback April

--- exercise28.js
+++ exercise28.js
@@ -1,23 +1,36 @@
+// Define an async object that contains the getAll method
 const async = {
+  // getAll method accepts an array of ajax calls (Promises) and a callback function
   getAll: async function (urlArray, callback) {
+    // Initialize an object to store the results of the ajax calls
     const results = {};
 
-    try {
-      const promises = urlArray.map((ajaxCall, index) => {
-        return ajaxCall().then((data) => {
-          results[index + 1] = data;
-        });
-      });
+    // Map the array of ajax calls to an array of promises
+    // Each promise resolves with the data or rejects with an error
+    const promises = urlArray.map((ajaxCall, index) =>
+      ajaxCall()
+        .then((data) => {
+          // On successful resolution of the promise, store the result
+          // as an object with 'status' and 'value' properties
+          results[index + 1] = { status: "fulfilled", value: data };
+        })
+        .catch((error) => {
+          // On rejection of the promise, store the error reason
+          // as an object with 'status' and 'reason' properties
+          results[index + 1] = { status: "rejected", reason: error };
+        })
+    );
 
-      await Promise.all(promises);
-      callback.call(results);
-    } catch (error) {
-      console.error("Error:", error);
-    }
+    // Wait for all promises to either fulfill or reject
+    // Promise.allSettled is used to ensure all promises are settled
+    await Promise.allSettled(promises);
+    // Call the callback function with the results object as its context
+    callback.call(results);
   },
 };
 
 // Example usage:
+// Define two example ajax calls as Promises
 const axCall1 = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -47,8 +60,11 @@ const axCall2 = () => {
     }, 500);
   });
 };
+
+// Define a callback function to handle the results of the ajax calls
 const callback = function () {
   console.log("Context:", this);
 };
 
+// Call the getAll method with the array of ajax calls and the callback function
 async.getAll([axCall1, axCall2], callback);
