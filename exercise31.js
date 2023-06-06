1134eb142cce278db96d8bdd5c391c3034c656e1
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-06-06 12:47:12 -0600

    Feedback June 2023

--- exercise31.js
+++ exercise31.js
@@ -6,19 +6,23 @@ document.addEventListener("DOMContentLoaded", function () {
       event.target.tagName === "P" &&
       event.target.textContent.indexOf("Toggle") !== -1
     ) {
-      // Get all even paragraphs (content paragraphs)
+      // Get all content paragraphs
       const contentParagraphs = document.querySelectorAll("p:nth-child(even)");
 
       // Hide all content paragraphs
       contentParagraphs.forEach((paragraph) => {
-        paragraph.classList.add("hidden");
+        // If the paragraph is not the next sibling of the clicked p element, hide it
+        if (paragraph !== event.target.nextElementSibling) {
+          paragraph.classList.add("hidden");
+        }
       });
 
       // Get the next sibling of the clicked p element
       let adjacentP = event.target.nextElementSibling;
 
-      // Remove the "hidden" class on the adjacent p element
-      adjacentP.classList.remove("hidden");
+      // Toggle the "hidden" class on the adjacent p element
+      // If it's currently hidden, it will be shown, and vice versa
+      adjacentP.classList.toggle("hidden");
     }
   });
 });
