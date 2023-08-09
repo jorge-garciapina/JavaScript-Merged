180c6d1b3caa38d1d4ee589001db473507760208
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-08-09 12:05:37 -0600

    Feedback August

--- exercise23.js
+++ exercise23.js
@@ -4,32 +4,38 @@
 // 'Xn' is the current value in the sequence, 'a' is the multiplier, 'c' is the increment, and 'm' is the modulus.
 // LCGs have certain properties and limitations, and the choice of parameters (a, c, and m) affects the quality of the generated sequence.
 
+// I have selected new values for m, a abd c
 // Our LCG generator factory function.
 function generateUniqueRandomNumbers(min, max) {
-  let m = max - min + 1; // Calculate the modulus based on the given range.
-  let a = 101; // Multiplier
-  let c = 103; // Increment
-  let seed = Math.floor(Math.random() * m); // Randomize the seed within the range.
-  let count = 0; // Counter to keep track of the number of generated values.
+  let m = 2 ** 32; // We'll use a big modulus to make use of good LCG parameters.
+  let a = 1664525;
+  let c = 1013904223;
+
+  let range = max - min + 1;
+  let seed = Math.floor(Math.random() * m);
+
+  let generated = new Set(); // Store already generated numbers within range.
 
   return function () {
-    if (count < 2 * m) {
-      // Check if the number of generated values is less than twice the modulus.
-      seed = (a * seed + c) % m; // LCG formula to calculate the next value.
-      let value = min + seed; // Map the value to the desired range.
-      count++;
-      return value; // Return the generated value.
-    } else {
-      return "Limit reached"; // Return the "Limit reached" message when the limit is reached.
+    let value;
+    do {
+      seed = (a * seed + c) % m;
+      value = min + (seed % range); // Map the value to the desired range.
+    } while (generated.has(value));
+
+    generated.add(value); // Remember the generated value.
+
+    if (generated.size >= range + 1) {
+      return "Limit reached";
     }
+
+    return value;
   };
 }
 
-const min = 5;
+const min = 1;
 const max = 20;
-
 const interval = generateUniqueRandomNumbers(min, max);
-
 for (let i = min; i < max; i++) {
-  console.log(interval()); // Print the generated numbers within the interval.
+  console.log(`i = ${i}`, interval());
 }
