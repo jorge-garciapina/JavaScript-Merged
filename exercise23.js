71cec83b33cbf0ed52a03fdbd71aa4b0d05edda7
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-08-31 16:46:14 -0600

    Feedback August 31st

--- exercise23.js
+++ exercise23.js
@@ -1,41 +1,99 @@
 // Introduction to the LCG:
 // A Linear Congruential Generator (LCG) is a type of pseudorandom number generator that generates a sequence of numbers
-// based on a linear equation. The equation is of the form: Xn+1 = (a * Xn + c) mod m.
-// 'Xn' is the current value in the sequence, 'a' is the multiplier, 'c' is the increment, and 'm' is the modulus.
-// LCGs have certain properties and limitations, and the choice of parameters (a, c, and m) affects the quality of the generated sequence.
+// based on a linear equation. The equation is of the form: Xn+1 = (a * Xn + b) mod m.
+// 'Xn' is the current value in the sequence, 'a' is the multiplier, 'b' is the increment, and 'm' is the modulus.
+// LCGs have certain properties and limitations, and the choice of parameters (a, b, and m) affects the quality of the generated sequence.
 
-// I have selected new values for m, a abd c
-// Our LCG generator factory function.
+// Function to compute the greatest common divisor of two numbers.
+function gcd(a, b) {
+  if (b === 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+}
+
+// Function to find all multiples of a number.
+function findMultiples(num) {
+  var multiples = [];
+  for (var i = Math.floor(num / 2); i >= 1; i--) {
+    if (num % i === 0) {
+      multiples.push(i);
+    }
+  }
+  return multiples;
+}
+
+// Function to check if a number is prime.
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+
+  return true;
+}
+
+// Function to find all prime multiples of a number.
+function findPrimeMultiples(num) {
+  var multiples = findMultiples(num);
+
+  var primeMultiples = multiples.filter((number) => {
+    return isPrime(number);
+  });
+  return primeMultiples;
+}
+
+// Function to compute the value of 'a' for the LCG.
+function computeA(m) {
+  const primeMultiples = findPrimeMultiples(m);
+  let a = primeMultiples.reduce((a, b) => {
+    return a * b;
+  }, 1);
+  if (m % 4 === 0) {
+    a = a * 4;
+  }
+  return a + 1;
+}
+
+// Function to generate unique random numbers within a range.
 function generateUniqueRandomNumbers(min, max) {
-  let m = 2 ** 32; // We'll use a big modulus to make use of good LCG parameters.
-  let a = 1664525;
-  let c = 1013904223;
+  let m = max - min + 1;
 
-  let range = max - min + 1;
+  // Check if 'b' and 'm' are coprime.
+  if (gcd(1, m) !== 1) {
+    return "b and m are not coprime";
+  }
+
+  let a = computeA(m);
+
+  let b = 1;
   let seed = Math.floor(Math.random() * m);
-
-  let generated = new Set(); // Store already generated numbers within range.
+  let count = 0;
 
   return function () {
-    let value;
-    do {
-      seed = (a * seed + c) % m;
-      value = min + (seed % range); // Map the value to the desired range.
-    } while (generated.has(value));
-
-    generated.add(value); // Remember the generated value.
-
-    if (generated.size >= range + 1) {
+    if (count < 2 * m) {
+      seed = (a * seed + b) % m;
+      let value = min + seed;
+      count++;
+      return value;
+    } else {
       return "Limit reached";
     }
-
-    return value;
   };
 }
 
 const min = 1;
-const max = 20;
+const max = 4;
+
 const interval = generateUniqueRandomNumbers(min, max);
-for (let i = min; i < max; i++) {
-  console.log(`i = ${i}`, interval());
+
+for (let i = min; i <= max; i++) {
+  console.log(`${i}: `, interval()); // Print the generated numbers within the interval.
 }
+
+
